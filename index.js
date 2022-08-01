3aa0933fad5864794add070058df89759861a0b0
jananthan93 <jjananthan93@gmail.com>
2022-08-01 08:05:03 +0530

    refresh issue for static web in server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express =require('express');
 const path = __dirname + '/client/build/';
 app.use(express.static(path));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path + 'index.html');
 });
 
